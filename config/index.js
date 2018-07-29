527b7affeddd2a6e6ac50aeee7ef452ce12a1df8
mingchenzz <947822283@qq.com>
2018-07-29 14:20:36 +0800

    4
    4

--- config/index.js
+++ config/index.js
@@ -15,8 +15,8 @@ module.exports = {
 
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    //host: 'localhost', // can be overwritten by process.env.HOST
+    //port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
